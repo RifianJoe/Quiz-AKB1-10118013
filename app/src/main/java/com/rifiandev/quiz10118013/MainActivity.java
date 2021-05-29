979936377d7c4195987f7538eaf96816267af8c6
Rifian Joe Premilenio <rjpremilenio@gmail.com>
2021-05-29 15:00:55 +0700

    Initial commit

---
+++ app/src/main/java/com/rifiandev/quiz10118013/MainActivity.java
@@ -0,0 +1,21 @@
+package com.rifiandev.quiz10118013;
+
+/*
+Tanggal Pengerjaan : Sabtu, 29 Mei 2021
+NIM : 10118013
+Nama : Rifian Joe Premilenio
+Kelas : IF-01
+ */
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
